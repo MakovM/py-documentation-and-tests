608f5e7be5aec478814ad6d597436e2146f77629
MakovM <makovinskiy09@gmail.com>
2025-09-13 13:33:46 +0300

    Solution

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -160,6 +160,32 @@ class MovieImageUploadTests(TestCase):
         self.assertIn("movie_image", res.data[0].keys())
 
 
+class JwtAuthFlowTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="jwtuser@test.com",
+            password="testpass123",
+            is_staff=True
+        )
+
+    def test_jwt_token_authentication(self):
+        res = self.client.post(
+            reverse("user:token_obtain_pair"),
+            {"email": "jwtuser@test.com", "password": "testpass123"},
+            format="json"
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn("access", res.data)
+
+        token = res.data["access"]
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+        res = self.client.get(MOVIE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+
 class UnauthenticatedBusApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
@@ -296,7 +322,6 @@ class AdminMovieTest(TestCase):
             "duration": 90,
         }
         res = self.client.post(MOVIE_URL, payload)
-        print(res.data)
         movie = Movie.objects.get(id=res.data["id"])
 
         for key in payload:
